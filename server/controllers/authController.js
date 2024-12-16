fb43bd1826d7ba0c66f3115523661117f2749422
pavithradevi-138 <pavithradevi138.dev@gmail.com>
2024-12-16 17:09:12 +0530

    User Registration Controller function created.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,8 +15,17 @@ export const Register = async(req, res) => {
       return res.json({success: false, message: 'User already exists!'})
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new UserModel({name, email, password: hashedPassword})
-    await newUser.save()
+    const user = new UserModel({name, email, password: hashedPassword})
+    await user.save()
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000  //convert the expiry time in ms
+    })
 
   } catch (error) {
     res.json({success: false, message: error})
